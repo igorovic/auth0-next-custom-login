620e41a0fe66eff6853e803c4a602d2ac4849011
igorovic <hello@dyve.dev>
2021-09-30 14:59:07 +0200

    wip

---
+++ pages/login.tsx
@@ -0,0 +1,21 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+
+const Login: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Login</title>
+      </Head>
+
+      <main>
+        <h1>Welcome</h1>
+      </main>
+
+      <footer></footer>
+    </div>
+  );
+};
+
+export default Login;
